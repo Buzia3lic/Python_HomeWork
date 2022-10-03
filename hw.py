952c38186b599e628a19774f62b9f14a128a27a6
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-03 14:27:08 +0300

    ex3 done

--- hw.py
+++ hw.py
@@ -1,15 +1,10 @@
-count = 1
-for i in range (0, 2):
-    for j in range (0, 2):
-        for k in range (0, 2):
-            print(f'Комбинация {count}:')
-            count += 1
-            print (i, j, k) 
-            cond1 = not (i or j or k)
-            cond2 = (not i) or (not j) or (not k)
-            print ('Результат для [not (i or j or k)]: ', cond1)
-            print ('Результат для [(not i) or (not j) or (not k)]: ',cond2)
-            print ('Сравнение этих двух утверждений: ', cond1 == cond2)
-            print ()
-            
-
+print('II  |  I')
+print('=========')
+print('III | IV')
+x = int(input('Введите координату X: '))
+y = int(input('Введите координату Y: '))
+if x == 0 or y == 0: print('Координаты не могут быть равны 0!')
+elif x > 0 and y > 0: print('Точка находится в I четверти')
+elif x > 0 and y < 0: print('Точка находится в IV четверти')
+elif x < 0 and y > 0: print('Точка находится в II четверти')
+elif x < 0 and y < 0: print('Точка находится в III четверти')
